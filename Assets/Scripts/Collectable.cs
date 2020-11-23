7a677116cdd19fa7b742283efc05721818e122ca
samj11 <samj11@gmail.com>
2020-11-23 10:03:36 -0500

    Implemented Collectable
    It communicate with a UI Manager to store how many collectables collected.

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private UIManager _UIManager;
+    private float _rotateSpeed = 1.2f;
+    private float _targetAngle = 160f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _UIManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+        if (!_UIManager)
+            Debug.Log("UI Manager is null");
+    }
+
+    private void Update()
+    {
+        IdleMovement();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            _UIManager.UpdateCollectableCount();
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void IdleMovement()
+    {
+        Vector3 rotationSpeed = new Vector3(2f, 2f, 2f);
+        transform.Rotate(rotationSpeed);
+
+        /*
+        Quaternion target = Quaternion.Euler(_targetAngle, _targetAngle, _targetAngle);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * _rotateSpeed);
+        */
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text _UICollectable;
+
+    [SerializeField]
+    private int _collectableCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _UICollectable.text = "Collectable : " + _collectableCount;
+    }
+
+    public void UpdateCollectableCount()
+    {
+        ++_collectableCount;
+        _UICollectable.text = "Collectable : " + _collectableCount;
+    }
+}
