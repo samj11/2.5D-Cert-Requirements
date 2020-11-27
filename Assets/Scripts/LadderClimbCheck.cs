084475aae782afb487e5937e52b9b31c90bb8fc6
samj11 <samj11@gmail.com>
2020-11-27 16:48:56 -0500

    Implemented Ladder System
    Can climb up and descend, go back to running

---
+++ Assets/Scripts/LadderClimbCheck.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderClimbCheck : MonoBehaviour
+{
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Player _player = other.GetComponent<Player>();
+            if (_player != null)
+                _player.LadderClimb();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Player _player = other.GetComponent<Player>();
+            if (_player != null)
+                _player.LadderExit();
+
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,8 +16,10 @@ public class Player : MonoBehaviour
     private Animator _animator;
     private bool _jumping;
     private bool _onLedge;
+    private bool _ladderClimbing;
 
     private LedgeGrabCheck _activeLedge;
+
 
     // Start is called before the first frame update
     void Start()
@@ -37,9 +39,7 @@ public class Player : MonoBehaviour
         if(_onLedge)
         {
             if(Input.GetKeyDown(KeyCode.E))
-            {
                 _animator.SetBool("ClimbingLedge", true);
-            }    
         }
 
     }
@@ -73,6 +73,35 @@ public class Player : MonoBehaviour
             }
         }
 
+        if(_ladderClimbing)
+        {
+            float v = Input.GetAxisRaw("Vertical");
+
+            if (v != 0)
+            {
+                _animator.enabled = true;
+                float climbingSpeed = 5f;
+                _direction = new Vector3(0, v, 0) * climbingSpeed;
+                _gravity = 0;
+
+                if (v>0)
+                {
+                    _animator.SetBool("OnGround", false);
+                    _animator.SetFloat("ClimbingLadder", Mathf.Abs(v));
+                }
+                else if (v<0)
+                    _animator.SetFloat("ClimbingLadder", -Mathf.Abs(v));
+            }
+            else if (v == 0)
+            {
+                _direction = new Vector3(0, v, 0);
+                _animator.enabled = false;
+            }
+
+            if (_charController.isGrounded)
+                LadderExit();
+        }
+
         _direction.y -= _gravity * Time.deltaTime;
 
         _charController.Move(_direction * Time.deltaTime);
@@ -98,4 +127,21 @@ public class Player : MonoBehaviour
         _animator.SetBool("LedgeGrab", false);
         _charController.enabled = true;
     }
+
+    public void LadderClimb()
+    {
+        float v = Input.GetAxisRaw("Vertical");
+
+        if (v != 0)
+            _ladderClimbing = true;
+    }
+
+    public void LadderExit()
+    {
+        _ladderClimbing = false;
+        _animator.SetBool("OnGround", true);
+        _animator.SetBool("ClimbingLadder", false);
+        _gravity = 1f;
+    }
+
 }
